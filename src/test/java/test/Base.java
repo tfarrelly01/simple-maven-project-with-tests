1f2131f4cdf177e6d1cffde88fd8d722889e58fb
Tony Farrelly <tfarrelly01@gmail.com>
2017-08-22 16:01:59 +0100

    changed interval for test failure

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -31,7 +31,8 @@ class Base {
 
     protected void run() {
         double r = Math.random();
-        if (r < 0.1) {
+    //    if (r < 0.1) {
+        if (r > 0.1) {
             fail("oops");
         } else if (r < 0.2) {
             throw new AssumptionViolatedException("skipping");
